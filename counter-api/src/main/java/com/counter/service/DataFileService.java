704e0597ea4d8730a91c6774ee0c6a7ac4dc661e
gyana12 <gyana.nk@gmail.com>
2024-05-15 12:45:41 +0530

    Delete counter-api directory

--- counter-api/src/main/java/com/counter/service/DataFileService.java
+++
@@ -1,9 +0,0 @@
-package com.navaneeth.counter.service;
-
-import java.util.List;
-
-public interface DataFileService {
-	
-
-	public List<String> getWordsList();		
- }
--- counter-api/src/main/java/com/counter/service/WordCountService.java
+++
@@ -1,8 +0,0 @@
-package com.navaneeth.counter.service;
-import java.util.Map;
-import java.util.List;
-
-public interface WordCountService {
-  	public String getTextsWithHighestCount(int topItems );									
-  	public Map<String,Integer>[] getWordCounts(List<String> words);					
-}
